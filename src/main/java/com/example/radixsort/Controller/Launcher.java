1542638535eccc7f509aa77cfd19a6aec0ca4657
swa <joshuagarrysalcedo@gmail.com>
2022-12-18 13:14:47 +0800

    save

---
+++ src/main/java/com/example/radixsort/Controller/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.radixsort.Controller;
+
+import com.example.radixsort.StartApplication;
+
+public class Launcher {
+    public static void main(String[] args) {
+        StartApplication.main(args);
+    }
+}
--- src/main/java/com/example/radixsort/Controller/ProgramController.java
+++ src/main/java/com/example/radixsort/Controller/ProgramController.java
@@ -1,23 +1,27 @@
 package com.example.radixsort.Controller;
 
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.value.ObservableBooleanValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
 
 import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class ProgramController {
+public class ProgramController  implements Initializable {
 
     @FXML
     private Button chooseFileBtn;
 
     @FXML
-    private TextField elementsTF;
+    private Spinner<Integer> elementsTF;
+
 
     @FXML
     private Button generateBtn;
@@ -29,10 +33,10 @@ public class ProgramController {
     private Button loadFileBtn;
 
     @FXML
-    private TextField maxTF;
+    private Spinner<Integer> maxTF;
 
     @FXML
-    private TextField minTF;
+    private Spinner<Integer> minTF;
 
     @FXML
     private AnchorPane pane;
@@ -66,6 +70,7 @@ public class ProgramController {
              arrayToSort = String.format("%s", f.getAbsoluteFile());
             f = new File(arrayToSort);
             textArea.setText("Selected File: " + arrayToSort);
+            showAlert("File selection complete!!", Alert.AlertType.INFORMATION);
         }
     }
 
@@ -75,33 +80,43 @@ public class ProgramController {
         String text = RadixSort.readTextFile(f, arrayToSort);
         arrayToSort = text;
         textArea.setText(RadixSort.formatString(arrayToSort));
+        showAlert("Load successful!", Alert.AlertType.INFORMATION);
     }
 
     @FXML
     void radixSortClick(ActionEvent event) {
-        System.out.println(arrayToSort);
-        int arr[] = RadixSort.StringtoArray(arrayToSort);
 
-        RadixSort.radixSort(arr, arr.length);
-        String output = RadixSort.arrayToString(arr);
-        RadixSort.saveTextToFile(output, "output.txt");
-        String finalOutput = String.format("UNSORTED ARRAY\n" +
-                "%s\nArray Sorted using radix sort\n%s", RadixSort.formatString(arrayToSort), RadixSort.formatString(output));
-        System.out.println(finalOutput);
-        textArea.setText(finalOutput);
+        try{
+            System.out.println(arrayToSort);
+            int arr[] = RadixSort.StringtoArray(arrayToSort);
+
+            RadixSort.radixSort(arr, arr.length);
+            String output = RadixSort.arrayToString(arr);
+            RadixSort.saveTextToFile(output, "output.txt");
+            String finalOutput = String.format("UNSORTED ARRAY\n" +
+                    "%s\nArray Sorted using radix sort\n%s", RadixSort.formatString(arrayToSort), RadixSort.formatString(output));
+            System.out.println(finalOutput);
+            textArea.setText(finalOutput);
+            showAlert("Sort successful!", Alert.AlertType.INFORMATION);
+        }catch (NullPointerException e) {
+            showAlert("No file to sort is selected!", Alert.AlertType.ERROR);
+            e.printStackTrace();
+        }
+
     }
 
     @FXML
     void onGenerateClick(ActionEvent event) {
         System.out.println("Generate click");
         if(elementsTF != null && minTF != null && maxTF != null) {
-            int length = Integer.parseInt(elementsTF.getText());
-            int min = Integer.parseInt(minTF.getText());
-            int max = Integer.parseInt(maxTF.getText());
+            int length = elementsTF.getValue();
+            int min = minTF.getValue();
+            int max = maxTF.getValue();
 
             String randomString = RadixSort.randomString(length, min, max);
             arrayToSort = randomString;
             textArea.setText(RadixSort.formatString(arrayToSort));
+            showAlert("Random numbers generated!", Alert.AlertType.INFORMATION);
         }
 
     }
@@ -110,6 +125,41 @@ public class ProgramController {
     void onSaveToInputClick(ActionEvent event) {
         System.out.println("On Save click");
         RadixSort.saveTextToFile(arrayToSort, "input.txt");
+        showAlert("Saved successful!", Alert.AlertType.INFORMATION);
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+            SpinnerValueFactory<Integer> valueOfElements = new SpinnerValueFactory.IntegerSpinnerValueFactory(1,50);
+            valueOfElements.setValue(10);
+            elementsTF.setValueFactory(valueOfElements);
+
+            SpinnerValueFactory<Integer> valueOfMin = new SpinnerValueFactory.IntegerSpinnerValueFactory(1,50);
+            valueOfMin.setValue(1);
+            minTF.setValueFactory(valueOfMin);
+
+
+            SpinnerValueFactory<Integer> valueOfMax = new SpinnerValueFactory.IntegerSpinnerValueFactory(51,100);
+            valueOfMax.setValue(100);
+            maxTF.setValueFactory(valueOfMax);
+
+            elementsTF.setEditable(true);
+            minTF.setEditable(true);
+            maxTF.setEditable(true);
+
+
+
+            generateBtn.disableProperty().bind(generateNumbersRadioBtn.selectedProperty().not());
+            elementsTF.disableProperty().bind(generateNumbersRadioBtn.selectedProperty().not());
+            minTF.disableProperty().bind(generateNumbersRadioBtn.selectedProperty().not());
+            maxTF.disableProperty().bind(generateNumbersRadioBtn.selectedProperty().not());
+            saveToInputBtn.disableProperty().bind(generateNumbersRadioBtn.selectedProperty().not());
+
+
+    }
+
+    public void showAlert(String message, Alert.AlertType type) {
+        Alert alert = new Alert(type, message);
+        alert.showAndWait();
+    }
 }
